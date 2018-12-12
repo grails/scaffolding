0dba00e53b3e91582565811ab6165ab5941c55a2
sdelamo <sergio.delamo@softamo.com>
2018-12-12 12:49:53 +0100

    Annotate Application with @PluginSource

--- grails-app/init/grails/plugin/scaffolding/Application.groovy
+++ grails-app/init/grails/plugin/scaffolding/Application.groovy
@@ -2,7 +2,11 @@ package grails.plugin.scaffolding
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import grails.plugins.metadata.PluginSource
+import groovy.transform.CompileStatic
 
+@CompileStatic
+@PluginSource
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application)

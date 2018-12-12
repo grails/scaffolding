14ca3d047526f04de46d5e594f1fbf260d2a6b1b
sdelamo <sergio.delamo@softamo.com>
2018-12-12 12:50:04 +0100

    grailsVersion 4.0.0.BUILD-SNAPSHOT > *

--- src/main/groovy/grails/plugin/scaffolding/ScaffoldingGrailsPlugin.groovy
+++ src/main/groovy/grails/plugin/scaffolding/ScaffoldingGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import org.grails.web.servlet.view.GroovyPageViewResolver
 class ScaffoldingGrailsPlugin extends Plugin {
 
    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.2.7 > *"
+    def grailsVersion = "4.0.0.BUILD-SNAPSHOT > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"

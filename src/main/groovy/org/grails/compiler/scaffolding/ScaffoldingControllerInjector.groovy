9c0d2b53c3d2028ddbaf77f96bcb854f93a3e8d3
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-08-30 08:39:32 -0700

    @ScaffoldController annotation support. Fixes #113

--- src/main/groovy/org/grails/compiler/scaffolding/ScaffoldingControllerInjector.groovy
+++ src/main/groovy/org/grails/compiler/scaffolding/ScaffoldingControllerInjector.groovy
@@ -2,6 +2,7 @@ package org.grails.compiler.scaffolding
 
 import grails.compiler.ast.AstTransformer
 import grails.compiler.ast.GrailsArtefactClassInjector
+import grails.plugin.scaffolding.annotation.ScaffoldController
 import grails.rest.RestfulController
 import groovy.transform.CompileStatic
 import org.codehaus.groovy.ast.ClassHelper
@@ -13,6 +14,7 @@ import org.grails.compiler.injection.GrailsASTUtils
 import org.grails.compiler.web.ControllerActionTransformer
 import org.grails.core.artefact.ControllerArtefactHandler
 import org.grails.plugins.web.rest.transform.ResourceTransform
+
 /**
  * Transformation that turns a controller into a scaffolding controller at compile time of 'static scaffold = Foo'
  * is specified
@@ -41,22 +43,26 @@ class ScaffoldingControllerInjector implements GrailsArtefactClassInjector {
     @Override
     void performInjectionOnAnnotatedClass(SourceUnit source, ClassNode classNode) {
         def propertyNode = classNode.getProperty(PROPERTY_SCAFFOLD)
+        def annotationNode = classNode.getAnnotations(ClassHelper.make(ScaffoldController)).find()
 
         def expression = propertyNode?.getInitialExpression()
-        if(expression instanceof ClassExpression) {
-
-            ClassNode superClassNode = ClassHelper.make(RestfulController).getPlainNodeReference()
+        if (expression instanceof ClassExpression || annotationNode) {
+            ClassNode superClassNode = ClassHelper.make(annotationNode?.getMember("value")?.type?.getTypeClass()?:RestfulController).getPlainNodeReference()
             def currentSuperClass = classNode.getSuperClass()
-            if(currentSuperClass.equals( GrailsASTUtils.OBJECT_CLASS_NODE )) {
-                def domainClass = ((ClassExpression) expression).getType()
+            if (currentSuperClass.equals(GrailsASTUtils.OBJECT_CLASS_NODE)) {
+                def domainClass = expression? ((ClassExpression) expression).getType() : null
+                if (!domainClass) {
+                    domainClass = annotationNode.getMember("domain")?.type
+                    if (!domainClass) {
+                        GrailsASTUtils.error(source, classNode, "Scaffolded controller (${classNode.name}) with @ScaffoldController does not have domain class set.", true)
+                    }
+                }
                 classNode.setSuperClass(GrailsASTUtils.nonGeneric(superClassNode, domainClass))
                 new ResourceTransform().addConstructor(classNode, domainClass, false)
-            }
-            else if( ! currentSuperClass.isDerivedFrom(superClassNode)) {
+            } else if (!currentSuperClass.isDerivedFrom(superClassNode)) {
                GrailsASTUtils.error(source, classNode, "Scaffolded controllers (${classNode.name}) cannot extend other classes: ${currentSuperClass.getName()}", true)
             }
-        }
-        else if(propertyNode != null) {
+        } else if (propertyNode != null) {
             GrailsASTUtils.error(source, propertyNode, "The 'scaffold' property must refer to a domain class.", true)
         }
     }

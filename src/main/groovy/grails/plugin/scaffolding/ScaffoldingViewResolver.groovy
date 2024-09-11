0c595e2e60bc5c4cedf2e7b4badd6d1b1b42435c
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-09-11 15:11:58 -0700

    Better to do 1 comparison instead of 2 every time

--- src/main/groovy/grails/plugin/scaffolding/ScaffoldingViewResolver.groovy
+++ src/main/groovy/grails/plugin/scaffolding/ScaffoldingViewResolver.groovy
@@ -79,7 +79,7 @@ class ScaffoldingViewResolver extends GroovyPageViewResolver implements Resource
         if (!res?.exists()) {
             def url = IOUtils.findResourceRelativeToClass(controllerClass, "/META-INF/templates/scaffolding/${shortViewName}.gsp")
             res = url? new UrlResource(url) : null
-            if (!res?.exists() && templateOverridePluginDescriptor) {
+            if (templateOverridePluginDescriptor && !res?.exists()) {
                 url = IOUtils.findResourceRelativeToClass(templateOverridePluginDescriptor, "/META-INF/templates/scaffolding/${shortViewName}.gsp")
                 res = url ? new UrlResource(url) : null
             }

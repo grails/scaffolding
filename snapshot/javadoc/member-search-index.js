f5409b6222bc6bc436ea95c17693e07a018b0fe3
Puneet Behl <behlp@objectcomputing.com>
2024-09-14 01:02:06 +0000

    Deploying to gh-pages - 01:02:06

--- snapshot/javadoc/member-search-index.js
+++ snapshot/javadoc/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"grails.plugin.scaffolding.annotation","c":"Scaffold","l":"domain()"},{"p":"grails.plugin.scaffolding.annotation","c":"Scaffold","l":"readOnly()"},{"p":"grails.plugin.scaffolding.annotation","c":"Scaffold","l":"value()"}]
+memberSearchIndex = [{"p":"grails.plugin.scaffolding.annotation","c":"Scaffold","l":"domain()"},{"p":"grails.plugin.scaffolding.annotation","c":"Scaffold","l":"readOnly()"},{"p":"grails.plugin.scaffolding.annotation","c":"Scaffold","l":"value()"}];updateSearchResults();

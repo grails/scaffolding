759930c6ab11bbedabde0dcb6e69a09b36075280
Puneet Behl <behlp@objectcomputing.com>
2024-09-14 01:30:15 +0000

    Deploying to gh-pages - 01:30:15

--- snapshot/javadoc/package-search-index.js
+++ snapshot/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"grails.plugin.scaffolding.annotation"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"grails.plugin.scaffolding.annotation"}];updateSearchResults();

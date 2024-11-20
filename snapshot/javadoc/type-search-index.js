e35e7d906916ab8d1773d18f1b61d781f85f0b02
grails-build <grails-build@users.noreply.github.com>
2024-11-20 11:56:45 +0000

    Deploying to gh-pages - 11:56:45

--- snapshot/javadoc/type-search-index.js
+++ snapshot/javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"grails.plugin.scaffolding.annotation","l":"Scaffold"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"grails.plugin.scaffolding.annotation","l":"Scaffold"}]
